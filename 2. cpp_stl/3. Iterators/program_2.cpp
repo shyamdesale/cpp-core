3119f3466337264393bd3cd48cbe9c560045ef3f
ShyamDesale <shyamdesale006@gmail.com>
2026-02-19 12:59:41 +0530

    array using iterators

--- 2. cpp_stl/3. Iterators/program_2.cpp
+++ 2. cpp_stl/3. Iterators/program_2.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
+#include<array>          // add this
 using namespace std;
 
 int main ()
 {
-    array<int , 5> arr = [1 , 3 , 5 , 7 , 9];
-    for(auto it : arr.begin() , it! = arr.end() , )
+    array<int , 5> arr = {1 , 3 , 5 , 7 , 9};
+
+    // iterator loop fix
+    for(auto it = arr.begin(); it != arr.end(); ++it)
+    {
+        cout << *it << " ";
+    }
 }
