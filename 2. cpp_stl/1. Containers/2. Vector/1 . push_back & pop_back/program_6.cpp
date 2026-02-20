6cd826928831fa4d1e76cda46e47dc4a48fe389d
ShyamDesale <shyamdesale006@gmail.com>
2026-02-20 16:03:25 +0530

    Copy using iterator range

---
+++ 2. cpp_stl/1. Containers/2. Vector/1 . push_back & pop_back/program_6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec1(4, 0);      // {0,0,0,0}
+    vector<int> vec2(4, 10);     // {10,10,10,10}
+
+    // Copy using iterator range
+    vector<int> vec3(vec2.begin(), vec2.end());
+
+    cout << "vec1: ";
+    for (int x : vec1) cout << x << " ";
+    cout << endl;
+
+    cout << "vec2: ";
+    for (int x : vec2) cout << x << " ";
+    cout << endl;
+
+    cout << "vec3: ";
+    for (int x : vec3) cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
