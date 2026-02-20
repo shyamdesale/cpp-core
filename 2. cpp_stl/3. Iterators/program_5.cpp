afa6c69c1c0c834922a5b047aa8f6f49e56fdd00
ShyamDesale <shyamdesale006@gmail.com>
2026-02-20 15:16:05 +0530

    Range-Based String Traversal Using STL in C++

--- 2. cpp_stl/3. Iterators/program_5.cpp
+++ 2. cpp_stl/3. Iterators/program_5.cpp
@@ -1,18 +1,18 @@
-// /* Range-Based String Traversal Using STL in C++ */
+/* Range-Based String Traversal Using STL in C++ */
 
-// #include <iostream>   // for cout
-// #include <string>     // for string class
-// using namespace std;
-// int main ()
-// {
-//     string s = "abcde";      // string initialize
-//     // range-based for loop: iterates over each character of the string
-//     for (auto c : s)
-//     {
-//         cout << c << " ";    // print each character with space
-//     }
-//     return 0;
-// }
+#include <iostream>   // for cout
+#include <string>     // for string class
+using namespace std;
+int main ()
+{
+    string s = "abcde";      // string initialize
+    // range-based for loop: iterates over each character of the string
+    for (auto c : s)
+    {
+        cout << c << " ";    // print each character with space
+    }
+    return 0;
+}
 
 
 
