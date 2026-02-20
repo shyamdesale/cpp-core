2d145b5237b08480d6d72ff8c9e37a035b8b0072
ShyamDesale <shyamdesale006@gmail.com>
2026-02-20 14:08:31 +0530

    Array Traversal Using Iterators (begin() 7 end()) in C++ STL

--- 2. cpp_stl/3. Iterators/program_2.cpp
+++ 2. cpp_stl/3. Iterators/program_2.cpp
@@ -9,27 +9,27 @@ int main ()
     array<int , 5> arr = {1 , 3 , 5 , 7 , 9};
 
     // iterator loop fix
-    for(auto it = arr.begin(); it != arr.end(); it++)
+    for(auto t = arr.begin(); t != arr.end(); t++)
     {
-        cout << *it << " ";
+        cout << *t << " ";
     }
 }
 
 //  Output : 1 3 5 7 9
 
-#include<iostream>
-#include<array>          // add this
-using namespace std;
+// #include<iostream>
+// #include<array>          // add this
+// using namespace std;
 
-int main ()
-{
-    array<int , 5> arr = {1 , 3 , 5 , 7};
+// int main ()
+// {
+//     array<int , 5> arr = {1 , 3 , 5 , 7};
 
-    // iterator loop fix
-    for(auto it = arr.begin(); it != arr.end(); ++it)
-    {
-        cout << *it << " ";
-    }
-}
+//     // iterator loop fix
+//     for(auto it = arr.begin(); it != arr.end(); ++it)
+//     {
+//         cout << *it << " ";
+//     }
+// }
 
-//  Output : 1 3 5 7 0
+// //  Output : 1 3 5 7 0
