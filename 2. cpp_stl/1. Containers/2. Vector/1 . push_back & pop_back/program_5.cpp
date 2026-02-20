71540a03e6910e49306024e6c7d65a76dd448ce9
ShyamDesale <shyamdesale006@gmail.com>
2026-02-20 15:55:01 +0530

    Undo Feature Simulation

---
+++ 2. cpp_stl/1. Containers/2. Vector/1 . push_back & pop_back/program_5.cpp
@@ -0,0 +1,24 @@
+// Undo Feature Simulation
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<string> history;
+
+    history.push_back("Type A");
+    history.push_back("Type B");
+    history.push_back("Delete A");
+
+    cout << "Current: " << history.back() << endl;
+
+    // Undo last operation
+    history.pop_back();
+
+    cout << "After Undo: " << history.back() << endl;
+}
+
+/*  Output :
+    Current: Delete A
+    After Undo: Type B
+*/
