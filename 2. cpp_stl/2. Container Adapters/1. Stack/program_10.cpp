bc112935e9f1e1a88b1c0385bd47287c45f70fcd
ShyamDesale <shyamdesale006@gmail.com>
2026-03-06 16:28:37 +0530

    Checking if Stack is Empty

--- 2. cpp_stl/2. Container Adapters/1. Stack/program_10.cpp
+++ 2. cpp_stl/2. Container Adapters/1. Stack/program_10.cpp
@@ -14,3 +14,24 @@ int main() {
 
     return 0;
 }
+
+
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+
+    stack<int> st;
+
+    st.push(50);
+
+    if(!st.empty()) {
+        cout << "Top element: " << st.top();
+    } else {
+        cout << "Stack is empty";
+    }
+
+    return 0;
+}
